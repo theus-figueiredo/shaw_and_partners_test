41bc460ab9dd6a265468a92503553d2ff393aeb6
theus-figueiredo <matheus.b.figueiredo@outlook.com>
2023-09-05 18:23:02 -0300

    started creating user controller

---
+++ src/controller/userController.ts
@@ -0,0 +1,30 @@
+import { Request, Response, response } from 'express';
+import userData from '../data/userData';
+import csvParser from 'csv-parser';
+
+class UserController {
+
+
+    public readCsvFile(req: Request, res: Response): Response {
+        try {
+            const { file } = req.body;
+
+            if(!file) {
+                return res.status(400).json({message: "file not found"});
+            }
+    
+            const stream = csvParser();
+    
+            stream.on('data', (row) => {
+                userData.push(row);
+            })
+    
+            return res.status(201).json({data: userData});
+        } catch (error) {
+            return res.status(400).json({error: error.message});
+        }
+    }
+}
+
+
+export default new UserController();
