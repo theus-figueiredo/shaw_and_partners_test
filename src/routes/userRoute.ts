3863c2efe29ac91144690ec241527ca15c7233da
theus-figueiredo <matheus.b.figueiredo@outlook.com>
2023-09-05 23:42:47 -0300

    created a function to check if uploads folder exists and create one if not, also changed the name of the saved csv file to a timestamp in order to avoid conflict

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express';
 import multer from 'multer';
 import userController from '../controller/userController';
+import fs from 'fs';
+import path from 'path';
+
+const uploadFolder = 'uploads/';
+
+if(!fs.existsSync(uploadFolder)) {
+    fs.mkdirSync(uploadFolder);
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/')
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname);
+        const timestamp = Date.now();
+        const originalName = file.originalname;
+        const extension = path.extname(originalName);
+
+        const uniqueFileName = `${timestamp}${extension}`
+
+        cb(null, uniqueFileName);
     }
 });
 
